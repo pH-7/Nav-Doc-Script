89ca571d3cbc2803007392092155a3adbf1d8d12
Pierre <ph_7@hotmail.be>
2012-03-13 00:29:42 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <?php
+#### About ####
+/**
+ * @author      SORIA Pierre-Henry
+ * @email       pierrehs@hotmail.com
+ * @link        http://github.com/pH-7
+ * @copyright   Copyright pH7 Script All Rights Reserved.
+ * @license     CC-BY - http://creativecommons.org/licenses/by/3.0/
+ */
+ 
 namespace PH7\Doc;
 define('PH7', 1);
 
