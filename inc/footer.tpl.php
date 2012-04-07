abdbbdfe366bfdbac382916c00fa90be429286a9
Pierre <ph_7@hotmail.be>
2012-04-07 17:42:50 +0300

    Some changes

--- inc/footer.tpl.php
+++ inc/footer.tpl.php
@@ -1,10 +1,17 @@
 <?php
 namespace PH7\Doc;
-defined('PH7') or exit('Restricted access'); 
+defined('PH7') or exit('Restricted access');
 ?>
+		  </article>
+      </section>
+    </div>
+    </div>
+    
+    <footer>
       <?php get_langs_html() ?>
-      
-	  </div>
+      <p>By <a href="http://cool-on-web.com">cool on Web</a></p>
+    </footer>
+
   </div>
 
  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
